89e29853918f21274328dea79d593a4496dc4b6f
Mujeeb02 <83205006+Mujeeb02@users.noreply.github.com>
2024-06-05 11:02:33 +0530

    rental pulse project intial commit

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import '@/assets/styles/global.css'
+
+export const metadata={
+    title:'PropertyPulse | Find the perfect rental',
+    description:'Find your dream rental property',
+    keywords:'rental,find rental, find properties',
+}
+const MainLayout = ({ children }) => {
+    return (
+        (<html lang='en'>
+            <body>
+                <div>
+                    {children}
+                </div>
+            </body>
+        </html>)
+    )
+}
+
+export default MainLayout
